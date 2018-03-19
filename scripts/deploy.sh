14f43dde6de707936d0d6ab34b60871e9e1e1fc8
mbarz <developer@muxe.de>
2018-03-19 17:37:53 +0100

    008 Deployment mit SSH

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# copy the public dir to the public dir on web
+echo 'copy to server...'
+scp -r public/ muxe@schedar.uberspace.de:/home/muxe/html/blog
+echo 'done. all files are copied to server. Nothing more to do.'
