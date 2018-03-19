bd631205ea3f32fe9d7231cbd339bc49a544005e
mbarz <developer@muxe.de>
2018-03-19 17:49:18 +0100

    fixed deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
 # copy the public dir to the public dir on web
 echo 'copy to server...'
-scp -r public/ muxe@schedar.uberspace.de:/home/muxe/html/blog
+scp -r public/* muxe@schedar.uberspace.de:/home/muxe/html/blog
 echo 'done. all files are copied to server. Nothing more to do.'
