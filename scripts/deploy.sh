9e4e1da7d5b8efa72900ebb44d299938740f61f6
Max-Richard Barz <mbarz@muxe.de>
2018-03-21 15:26:52 +0100

    10 deploy server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,8 @@
 # copy the public dir to the public dir on web
 echo 'copy public dir to server...'
 rsync -rav -e ssh --progress --delete public/. muxe@schedar.uberspace.de:/home/muxe/html/blog/
-# scp -r public/. muxe@schedar.uberspace.de:/home/muxe/html/blog/
 
-#echo 'copy api logic'
+echo 'copy api logic'
 rsync -rav -e ssh --exclude node_modules server/. muxe@schedar.uberspace.de:/home/muxe/api/blog
 ssh muxe@schedar.uberspace.de 'source .bash_profile && svc -d ~/service/blog && cd /home/muxe/api/blog && npm install --production && svc -u ~/service/blog'
 
