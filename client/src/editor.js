8a1b4ba3914cf41e722cb0ca894c4c4c1d49cebd
muxe <mbarz@muxe.de>
2018-04-09 10:44:03 +0200

    babel

--- client/src/editor.js
+++ client/src/editor.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
-import { layout } from "./layout";
+import $ from 'jquery';
+import { layout } from './layout';
 
 var id = undefined;
 
@@ -35,33 +35,33 @@ export function renderEditor() {
   </form>
   `);
 
-  $("form.blog-post-editor").submit(e => {
+  $('form.blog-post-editor').submit(e => {
     e.preventDefault();
     if (id) update();
     else create();
     return false;
   });
 
-  $("#delete-button").click(() => {
-    console.log("delete");
-    const sure = confirm("Are you sure?");
+  $('#delete-button').click(() => {
+    console.log('delete');
+    const sure = confirm('Are you sure?');
     if (sure)
       fetch(`api/posts/${id}`, {
-        method: "DELETE",
-        credentials: "same-origin"
+        method: 'DELETE',
+        credentials: 'same-origin'
       }).then(response => {
         if (response.ok) showCreationForm();
-        else alert("deletion failed");
+        else alert('deletion failed');
       });
   });
 
-  fetch("api/loggedin", { credentials: "same-origin" })
+  fetch('api/loggedin', { credentials: 'same-origin' })
     .then(response => response.json())
     .then(data => {
       if (data.isAuthenticated) load();
       else {
-        console.log("not logged in");
-        $("#main").html(
+        console.log('not logged in');
+        $('#main').html(
           `<div class="message error-message">You need to log in to edit blog posts<br /><a href="login">Login</a></div>`
         );
       }
@@ -69,7 +69,7 @@ export function renderEditor() {
 }
 
 async function load() {
-  const selector = $("#post-selector");
+  const selector = $('#post-selector');
   selector.change(() => {
     const id = selector.val();
     if (id) loadPost(id);
@@ -82,21 +82,21 @@ async function load() {
 }
 
 async function reloadList() {
-  const selector = $("#post-selector");
+  const selector = $('#post-selector');
   const selected = selector.val();
-  selector.html("");
+  selector.html('');
 
-  const o = $("<option>");
+  const o = $('<option>');
   o.val(undefined);
   o.text(`new`);
   selector.append(o);
 
-  const list = await fetch("/api/posts", {
-    credentials: "same-origin"
+  const list = await fetch('/api/posts', {
+    credentials: 'same-origin'
   }).then(response => response.json());
   const divs = list
     .map(item => {
-      const o = $("<option>");
+      const o = $('<option>');
       o.val(item.id);
       o.text(`${item.id} - ${item.date} - ${item.title.substring(0, 10)}...`);
       return o;
@@ -108,7 +108,7 @@ async function reloadList() {
 
 function loadPost(id) {
   return fetch(`/api/posts/${id}`, {
-    credentials: "same-origin"
+    credentials: 'same-origin'
   })
     .then(response => {
       if (response.ok) return response.json();
@@ -123,64 +123,65 @@ function loadPost(id) {
 function showPost(post) {
   reloadList();
   id = post.id;
-  history.pushState({}, "", "edit?id=" + id);
-  $("#header").text(`Edit Post ${id}`);
-  $("#post-title").val(post.title);
-  $("#post-date").val(post.date);
-  $("#post-content").val(post.content);
-  $("#post-content").val(post.content);
-  $("#post-public")[0].checked = post.public;
+  history.pushState({}, '', 'edit?id=' + id);
+  $('#header').text(`Edit Post ${id}`);
+  $('#post-title').val(post.title);
+  $('#post-date').val(post.date);
+  $('#post-content').val(post.content);
+  $('#post-content').val(post.content);
+  $('#post-public')[0].checked = post.public;
 }
 
 function showCreationForm() {
   reloadList();
   id = undefined;
-  history.pushState({}, "", "edit");
-  $("#post-title").val("");
-  $("#post-date").val(new Date().toISOString().substring(0, 10));
-  $("#post-content").val("");
-  $("#header").text(`Create new Post`);
-  $("#post-public")[0].checked = true;
+  history.pushState({}, '', 'edit');
+  $('#post-title').val('');
+  $('#post-date').val(new Date().toISOString().substring(0, 10));
+  $('#post-content').val('');
+  $('#header').text(`Create new Post`);
+  $('#post-public')[0].checked = true;
 }
 
 function update() {
-  const values = { ...getFormValues(), id };
+  const formValues = getFormValues();
+  const values = { ...formValues, id };
   fetch(`api/posts/${id}`, {
-    method: "PUT",
-    credentials: "same-origin",
+    method: 'PUT',
+    credentials: 'same-origin',
     body: JSON.stringify(values),
     headers: new Headers({
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     })
   }).then(response => {
-    if (!response.ok) alert("update failed");
-    else alert("update successfully done");
+    if (!response.ok) alert('update failed');
+    else alert('update successfully done');
   });
 }
 
 function create() {
   const values = getFormValues();
   fetch(`api/posts`, {
-    method: "POST",
-    credentials: "same-origin",
+    method: 'POST',
+    credentials: 'same-origin',
     body: JSON.stringify(values),
     headers: new Headers({
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     })
   }).then(response => {
-    if (!response.ok) alert("creation failed");
+    if (!response.ok) alert('creation failed');
     else {
-      alert("update successfully done");
+      alert('update successfully done');
       response.json().then(post => showPost(post));
     }
   });
 }
 
 function getFormValues() {
-  const title = $("#post-title").val();
-  const date = $("#post-date").val();
-  const content = $("#post-content").val();
-  const isPublic = $("#post-public")[0].checked;
+  const title = $('#post-title').val();
+  const date = $('#post-date').val();
+  const content = $('#post-content').val();
+  const isPublic = $('#post-public')[0].checked;
   return { title, date, content, public: isPublic };
 }
 
@@ -188,8 +189,8 @@ function urlParams() {
   const url = decodeURIComponent(window.location.search.substring(1));
   const params = {};
   url
-    .split("&")
-    .map(item => item.split("="))
+    .split('&')
+    .map(item => item.split('='))
     .map(arr => ({ name: arr[0], value: arr[1] }))
     .forEach(item => (params[item.name] = item.value));
   return params;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-import "./style.css";
-import "./prism.css";
-import { render } from "./renderer";
+import './style.css';
+import './prism.css';
+import { render } from './renderer';
 
 render();
