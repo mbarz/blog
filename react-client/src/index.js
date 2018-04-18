5aacb8f83697db15d656a4321da801f342bc77af
mbarz <developer@muxe.de>
2018-04-18 15:44:53 +0200

    layout with react

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 const title = 'My Minimal Webpack Babel Setup';
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
+ReactDOM.render(
+  <div id="main">
+    <header>
+      <h1>muxes dev blog</h1>
+      <div className="spacer" />
+      <a href="/">
+        <i className="fa fa-list" />
+      </a>
+      <a href="/login">
+        <i className="fa fa-lock-open" />
+      </a>
+      <img src="https://travis-ci.org/mbarz/blog.svg?branch=master" />
+    </header>
+    <div className="posts" />
+    <div className="load-more-button">Mehr laden</div>
+  </div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
--- react-client/webpack.config.js
+++ react-client/webpack.config.js
@@ -1,11 +1,21 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['./src/index.js', 'react-hot-loader/patch'],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
       }
     ]
   },
@@ -17,7 +27,12 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 };
