353c952b78b529364c5ae262fa3ffb5b5703fb68
mbarz <developer@muxe.de>
2018-06-04 22:19:27 +0200

    GET, POST, DELETE for example are working

---
+++ php/app/product/delete.php
@@ -0,0 +1,31 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object file
+include_once '../config/database.php';
+include_once '../objects/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+$product->id = isset($_GET['id']) ? $_GET['id'] : die();
+ 
+if($product->delete()){
+  echo '{';
+  echo '  "message": "Product with id ' . $product->id . ' was deleted."';
+  echo '}';
+}
+ 
+// if unable to delete the product
+else{
+  echo '{';
+  echo '  "message": "Unable to delete object."';
+  echo '}';
+}
+?>
---
+++ php/app/product/read_one.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+ 
+$product->id = isset($_GET['id']) ? $_GET['id'] : die();
+ 
+$product->readOne();
+ 
+// create array
+$product_arr = array(
+  "id" =>  $product->id,
+  "name" => $product->name,
+  "description" => $product->description,
+  "price" => $product->price,
+  "category_id" => $product->category_id,
+  "category_name" => $product->category_name
+);
+ 
+// make it json format
+if($product->id)
+  print_r(json_encode($product_arr));
+else {
+  http_response_code(404);
+  echo '{"message":"Not Found"}';
+}
+?>
